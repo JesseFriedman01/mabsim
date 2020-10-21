65122f5a11ca10ee23e82846523f8fb93c9c8b44
Jesse Friedman <jessefriedman1031@gmail.com>
2020-10-21 09:53:49 -0400

    mac transfer

--- react_frontend/src/UI/sideDrawer.js
+++ react_frontend/src/UI/sideDrawer.js
@@ -5,22 +5,39 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import LaunchIcon from '@material-ui/icons/Launch';
 import HistoryIcon from '@material-ui/icons/History';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import { makeStyles } from '@material-ui/core/styles';
 import {Link} from "react-router-dom"
 import Divider from '@material-ui/core/Divider';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: 250,
+  },
+  mainListItem: {
+    fontWeight: '550',
+    fontFamily: 'Arial',
+    fontSize: '16px'
+
+  },
+  nestedListItem: {
+    paddingTop: theme.spacing(0),
+    paddingLeft: theme.spacing(5),
+    fontFamily: 'Arial',
+    fontSize: '16px'
   },
 }));
 
 
 export default function SideDrawer(props) {
     const [shouldBeOpen, setShouldBeOpen] = React.useState(props.openSideBar)
+    const [gettingStartedOpen, setGettingStartedOpen] = React.useState(false)
     const classes = useStyles();
 
     useEffect(() => {
@@ -32,6 +49,10 @@ export default function SideDrawer(props) {
         props.setStatus(false)
     }
 
+    const handleGettingStartedClick = () => {
+        setGettingStartedOpen(!gettingStartedOpen);
+    };
+
     return (
           <div className={classes.root}>
               <Drawer
@@ -41,28 +62,31 @@ export default function SideDrawer(props) {
               >
                 <div className={classes.root}>
                  <List component="nav">
-                    <ListItem button onClick={handleClose} component={Link} to="/">
-                      <ListItemIcon>
-                        <HelpOutlineIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Getting Started" />
+                    <ListItem button onClick={handleGettingStartedClick}>
+                      <ListItemText disableTypography className={classes.mainListItem} primary="Getting Started"/>
+                       {gettingStartedOpen ? <ExpandLess /> : <ExpandMore />}
                      </ListItem>
+                     <Collapse in={gettingStartedOpen} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                          <ListItem button className={classes.nestedListItem}>
+                            <ListItemText disableTypography primary="Usage" />
+                          </ListItem>
+                          <ListItem button className={classes.nestedListItem}>
+                            <ListItemText disableTypography primary="Learn" />
+                          </ListItem>
+                          <ListItem button className={classes.nestedListItem}>
+                            <ListItemText disableTypography primary="Support" />
+                          </ListItem>
+                        </List>
+                     </Collapse>
                   </List>
                   <Divider />
                   <List component="nav">
                     <ListItem button onClick={handleClose} component={Link} to="/create">
-                      <ListItemIcon>
-                        <LaunchIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Create New Sim" />
+                      <ListItemText disableTypography className={classes.mainListItem} primary="Create Sim" />
                      </ListItem>
-                  </List>
-                  <List component="nav">
                     <ListItem button onClick={handleClose} component={Link} to="/load">
-                      <ListItemIcon>
-                        <HistoryIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Load Existing Sim" />
+                      <ListItemText disableTypography className={classes.mainListItem} primary="Load Sim" />
                      </ListItem>
                   </List>
                 </div>
